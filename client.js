4de28dc8c9d14c86bde3b97a86b133ec530a0d95
Malik G <Malik@Malik.local>
2018-10-07 23:11:13 -0500

    completed assignment, stretch incomplete

---
+++ client.js
@@ -0,0 +1,83 @@
+
+$(document).ready(readyNow);
+   
+let employeeArray = [];
+let monthly = 0;
+
+class EmployeeInfo {
+   constructor(first, last, id, title, salary){
+      this.first = first;
+      this.last = last;
+      this.id = id;
+      this.title = title;
+      this.salary = salary;
+   }
+}
+
+//Set up button click events
+function readyNow(){
+   $('#submitButton').on('click', submitForm);
+   $('table').on('click', 'button', deleteEmployee);
+   
+}
+
+//Turn the input values into properties of an EmployeeInfo object and push into employeesArray
+function submitForm(){
+   event.preventDefault();
+   let fName = $('#firstNameInput').val();
+   let lName = $('#lastNameInput').val();
+   let empId = $('#idInput').val();
+   let empTitle = $('#titleInput').val();
+   let empSalary = Number($('#salaryInput').val().replace(/,/g, ''));
+   let newObj = new EmployeeInfo(fName, lName, empId, empTitle, empSalary)
+   $('tbody').append( `<tr>
+      <td>${newObj.first}</td>
+      <td>${newObj.last}</td>
+      <td>${newObj.id}</td>
+      <td>${newObj.title}</td>
+      <td>$${newObj.salary}</td>
+      <td><button id="delete">Delete</button></td></tr>`);
+   employeeArray.push(newObj);
+   emptyInputs();
+   increaseMonthly(empSalary/12);
+}
+
+function deleteEmployee(){
+   console.log('delete working');
+   let targetSalary = $(this).parent().prev(); //target the nearest sibling of the button's parent
+   let getSalary = parseInt(targetSalary.text().replace(/\$/g, '')); //get text from salary td, convert to a number
+   console.log(getSalary);
+   decreaseMonthly(getSalary/12);
+   $(this).parentsUntil('tbody').remove();
+}
+
+function increaseMonthly(num){
+   monthly += num;
+   console.log(monthly);
+   emptyAppendMonthly();
+}
+
+function decreaseMonthly(num) {
+   monthly -= num;
+   console.log(monthly);
+   emptyAppendMonthly();
+}
+
+function emptyAppendMonthly(){
+   $('#monthlyCosts').empty();
+   //$('#monthlyCosts').append(`<td id="monthlyCosts" colspan="6">Total Monthly Costs: $${monthly.toFixed(2)}</td>`);
+   if(monthly > 20000){
+      $('#monthlyCosts').append(`<td id="monthlyCosts" class="red" colspan="6">Total Monthly Costs: $${monthly.toFixed(2)}</td>`);
+   }
+   else {
+      $('#monthlyCosts').append(`<td id="monthlyCosts" colspan="6">Total Monthly Costs: $${monthly.toFixed(2)}</td>`);
+   }
+}
+
+function emptyInputs(){
+   $('#firstNameInput').val('');
+   $('#lastNameInput').val('');
+   $('#idInput').val('');
+   $('#titleInput').val('');
+   $('#salaryInput').val('');
+}
